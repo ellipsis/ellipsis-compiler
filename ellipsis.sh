3ba6d9c8d29da9ba12c6c70d13a1444bdf745bd5
G. Roggemans <g.roggemans@grog.be>
2016-04-16 01:03:54 +0200

    Added `ellipsis-compile` command alias

--- ellipsis.sh
+++ ellipsis.sh
@@ -3,12 +3,14 @@
 
 pkg.link() {
     fs.link_file "$PKG_PATH/bin/ellipsis-compiler" "$ELLIPSIS_PATH/bin/ellipsis-compiler"
+    fs.link_file "$PKG_PATH/bin/ellipsis-compiler" "$ELLIPSIS_PATH/bin/ellipsis-compile"
 }
 
 ##############################################################################
 
 pkg.unlink() {
     rm "$ELLIPSIS_PATH/bin/ellipsis-compiler"
+    rm "$ELLIPSIS_PATH/bin/ellipsis-compile"
 }
 
 ##############################################################################
