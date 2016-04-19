04f4991da8ae1708309892827311cb6e5b0bf744
G. Roggemans <g.roggemans@grog.be>
2016-04-19 07:48:48 +0200

    Added rough if-then-else parsing (first part)

--- src/compiler.bash
+++ src/compiler.bash
@@ -15,6 +15,13 @@ EC_OUTPUT="tmp.file"
 
 ##############################################################################
 
+compiler.print_error() {
+    msg.print "Syntax error in $file at line nr $line_nr:"
+    msg.print "    '$line'"
+}
+
+##############################################################################
+
 compiler.get_keyword() {
     local line="$1"
     cut -d ' ' -f2 <<< "$line"
@@ -35,37 +42,91 @@ compiler.compile() {
 compiler.parse_file() {
     local file="$1"
 
-    # Flags
-    local output=true
+    # Reset line number before parsing file
+    line_nr=0
 
-    local line_nr=0
+    # Parse file
     while read line; do
-        let line_nr=line_nr+1
-        if [[ "$line" =~ ^"$EC_COMMENT$EC_PROMPT".* ]]; then
-            local keyword="$(compiler.get_keyword "$line")"
-            msg.print "keyword : $keyword"
-            case $keyword in
-                include)
-                    local inc_file="$(cut -d ' ' -f3 <<< "$line")"
-                    compiler.parse_file "$inc_file"
-                    ;;
-                include_raw)
-                    local inc_file="$(cut -d ' ' -f3 <<< "$line")"
-                    compiler.include_raw "$inc_file"
-                    ;;
-                *)
-                    msg.print "Syntax error in $file at line nr $line_nr:"
-                    msg.print "    '$line'"
-                    ;;
-                esac
-        elif [[ "$line" =~ ^"$EC_COMMENT".* ]] ||\
-             [[ "$line" =~ ^$ ]]; then
-            # Ignore commented and empty lines
-            :
-        elif [ "$output" ]; then
-            echo "$line" >> "$EC_OUTPUT"
+        local output=1
+        compiler.parse_line "$line"
+        if [ "$?" -eq 1 -o "$?" -eq 2 ]; then
+            compiler.print_error
+            msg.print "    'fi' without matching 'if'"
+            exit 1
         fi
     done < "$file"
+}
+
+##############################################################################
+
+compiler.parse_if() {
+    output="$1"
+
+    while read line; do
+        compiler.parse_line "$line"
+        local ret="$?"
+
+        if [ "$ret" -eq 1 ]; then
+            return
+        elif [ "$ret" -eq 2 ]; then
+            if [ "$output" -eq 1 ]; then
+                output=0
+            else
+                output=1
+            fi
+        fi
+    done
+
+    msg.print "Syntax error in $file, missing 'fi'"
+    exit 1
+}
+
+##############################################################################
+
+compiler.parse_line() {
+    # Count parsed lines
+    let line_nr=line_nr+1
+
+    if [[ "$line" =~ ^"$EC_COMMENT$EC_PROMPT".* ]]; then
+        local keyword="$(compiler.get_keyword "$line")"
+        msg.print "keyword : $keyword"
+        case $keyword in
+            include)
+                # Get file name
+                local inc_file="$(cut -d ' ' -f3 <<< "$line")"
+
+                # Keep line_nr in current file and process include
+                local tmp_line_nr="$line_nr"
+                compiler.parse_file "$inc_file"
+                line_nr="$tmp_line_nr"
+                ;;
+            include_raw)
+                # Get file name
+                local inc_file="$(cut -d ' ' -f3 <<< "$line")"
+
+                compiler.include_raw "$inc_file"
+                ;;
+            if)
+                compiler.parse_if 1
+                ;;
+            else)
+                return 2
+                ;;
+            fi)
+                return 1
+                ;;
+            *)
+                compiler.print_error
+                exit 1
+                ;;
+            esac
+    elif [[ "$line" =~ ^"$EC_COMMENT".* ]] ||\
+            [[ "$line" =~ ^$ ]]; then
+        # Ignore commented and empty lines
+        :
+    elif [ "$output" -eq 1 ]; then
+        echo "$line" >> "$EC_OUTPUT"
+    fi
 }
 
 ##############################################################################
