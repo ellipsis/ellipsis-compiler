385db9372ff6c00086ae87d8ec2ebe9efa2dacb5
G. Roggemans <g.roggemans@grog.be>
2016-06-19 14:23:31 +0200

    v0.0.2-Alpha

--- src/version.bash
+++ src/version.bash
@@ -8,7 +8,7 @@ source "$ELLIPSIS_SRC/version.bash"
 ##############################################################################
 
 # Extension version
-ELLIPSIS_XVERSION="0.0.1-Alpha"
+ELLIPSIS_XVERSION="0.0.2-Alpha"
 # Ellipsis version dependency
 ELLIPSIS_VERSION_DEP="1.6.0"
 
